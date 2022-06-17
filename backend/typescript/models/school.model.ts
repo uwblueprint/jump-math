c4be79319a4c8cc7ac60b73d6a487aeaf13650de
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-06-16 20:31:51 -0400

    Add School Document Model

---
+++ backend/typescript/models/school.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+/**
+ * This document contains information about a single school.
+ */
+export interface School extends Document {
+  /** the unique identifier for the school */
+  id: string;
+  /** the name of the school */
+  name: string;
+  /** the country the school is located in */
+  country: string;
+  /** the city the school is located in */
+  city: string;
+  /** the address of the school */
+  address: string;
+  /** the teachers that teach at the school (reference to the IDs in the User table) */
+  teachers: [string];
+}
+
+const SchoolSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  teachers: {
+    type: [String],
+    required: true,
+  },
+});
+
+export default mongoose.model<School>("School", SchoolSchema);
