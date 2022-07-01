badf081630a07bced5ee0ac7732354a1e9b3da5c
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2022-06-30 20:46:09 -0400

    Add School Document Model (#1)
    * Add School Document Model
    
    * Add Missing Field
    
    * Fix Reference To Teacher

---
+++ backend/typescript/models/school.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+/**
+ * This document contains information about a single school.
+ */
+export interface School extends Document {
+  /** the unique identifier for the school */
+  id: string;
+  /** the name of the school */
+  name: string;
+  /** the country the school is located in */
+  country: string;
+  /** the sub-region the school is located in */
+  subRegion: string;
+  /** the city the school is located in */
+  city: string;
+  /** the address of the school */
+  address: string;
+  /** the teachers that teach at the school (reference to the IDs in the User table) */
+  teachers: [string];
+}
+
+const SchoolSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  subRegion: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  teachers: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    required: true,
+  },
+});
+
+export default mongoose.model<School>("School", SchoolSchema);
