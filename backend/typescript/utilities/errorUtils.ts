eb5473305734bdc755a696108a53999d8e101da3
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2022-06-30 21:16:12 -0400

    remove unneeded files

--- backend/typescript/utilities/errorUtils.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable-next-line import/prefer-default-export */
-export const getErrorMessage = (error: unknown): string => {
-  return error instanceof Error ? error.message : "Unknown error occurred.";
-};
