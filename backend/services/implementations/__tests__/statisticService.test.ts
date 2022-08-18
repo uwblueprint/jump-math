1a1a79c71f751724dc6b39d085405ba86df35f65
Mahad Zaryab <mahadzaryab1@gmail.com>
2022-08-18 15:26:42 -0400

    Add Test Case For New Service

---
+++ backend/services/implementations/__tests__/statisticService.test.ts
@@ -0,0 +1,81 @@
+import StatisticService from "../statisticService";
+
+import db from "../../../testUtils/testDb";
+
+import { GradingStatus } from "../../../models/testSession.model";
+import { createTestSessionWithSchoolAndResults } from "../../../testUtils/testSession";
+import { mockTestWithId } from "../../../testUtils/tests";
+import { createSchoolWithCountry } from "../../../testUtils/school";
+import { TestStatistic } from "../../interfaces/statisticService";
+
+describe("mongo statisticService", (): void => {
+  let statisticService: StatisticService;
+
+  beforeAll(async () => {
+    await db.connect();
+  });
+
+  afterAll(async () => {
+    await db.disconnect();
+  });
+
+  beforeEach(async () => {
+    statisticService = new StatisticService();
+  });
+
+  afterEach(async () => {
+    await db.clear();
+  });
+
+  it("testing stats service", async () => {
+    // insert school with given country
+    const school = await createSchoolWithCountry("Canada");
+    // create mock results
+    // average of two results is (100 + 0) / 2 = 50
+    // average for single question is (100 + 0) / 2 = 50
+    const results = [
+      {
+        student: "student-1",
+        score: 0,
+        answers: [10.5],
+        breakdown: [false],
+        gradingStatus: GradingStatus.GRADED,
+      },
+      {
+        student: "some-student-name",
+        score: 100,
+        answers: [11.5],
+        breakdown: [true],
+        gradingStatus: GradingStatus.GRADED,
+      },
+      // ungraded results get filtered out
+      {
+        student: "some-student-name",
+        score: 0,
+        answers: [],
+        breakdown: [],
+        gradingStatus: GradingStatus.UNGRADED,
+      },
+    ];
+    // create test session with specified school and results
+    await createTestSessionWithSchoolAndResults(school.id, results);
+
+    const expectedResult = new Map<string, TestStatistic>([
+      [
+        "Canada",
+        {
+          averageScore: 50,
+          averageQuestionScores: [
+            {
+              averageScore: 50,
+            },
+          ],
+        },
+      ],
+    ]);
+    const actualResult = await statisticService.getTestGradeStatisticsByCountry(
+      mockTestWithId.id,
+    );
+    expect(actualResult).toEqual(expectedResult);
+  });
+});
