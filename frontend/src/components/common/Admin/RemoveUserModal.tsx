5d66cefefc1937c672cd389f51f9fcc020c41aa7
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2022-10-16 12:31:13 -0400

    clean up code

--- frontend/src/components/common/Admin/RemoveUserModal.tsx
+++ frontend/src/components/common/Admin/RemoveUserModal.tsx
@@ -10,7 +10,8 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { ADMIN_PAGE } from "../../../constants/Routes";
 import { CloseOutlineIcon } from "../icons";
-import ModalText from "./ModalText";
+import ModalText from "../ModalText";
+import RemoveUserConfirmationModal from "./RemoveUserConfirmationModal";
 
 interface RemoveUserModalProps {
   name: string;
@@ -24,14 +25,11 @@ const RemoveUserModal = ({
   onCloseParent,
 }: RemoveUserModalProps): React.ReactElement => {
   const { onOpen, onClose, isOpen } = useDisclosure();
-  const [removed, setRemoved] = React.useState(false);
+  const [showConfirmation, setShowConfirmation] = React.useState(false);
 
-  const history = useHistory();
-  const navigateTo = () => history.push(ADMIN_PAGE);
-
-  const removeUser = () => {
+  const onRemoveUserClick = () => {
     console.log("Remove user with email: ", email);
-    setRemoved(true);
+    setShowConfirmation(true);
   };
 
   return (
@@ -47,18 +45,8 @@ const RemoveUserModal = ({
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent alignItems="center" p={2}>
-          {removed ? (
-            <>
-              <ModalText
-                header="The user has been removed from the Jump Math Database"
-                body={["The data has been removed securely."]}
-              />
-              <ModalFooter my={3}>
-                <Button variant="primary" mr={2} onClick={navigateTo}>
-                  Return to Database
-                </Button>
-              </ModalFooter>
-            </>
+          {showConfirmation ? (
+            <RemoveUserConfirmationModal />
           ) : (
             <>
               <ModalText
@@ -81,7 +69,7 @@ const RemoveUserModal = ({
                 <Button
                   rightIcon={<CloseOutlineIcon />}
                   variant="primary"
-                  onClick={removeUser}
+                  onClick={onRemoveUserClick}
                 >
                   Remove User
                 </Button>
