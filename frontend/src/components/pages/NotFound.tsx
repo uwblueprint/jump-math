d2e6259f8de95d7a572aed7715f644751da5c51c
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2022-10-23 23:54:29 -0400

    update page design

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Button, VStack, Text } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+import { HOME_PAGE } from "../../constants/Routes";
 
 const NotFound = (): React.ReactElement => {
+  const history = useHistory();
+  const navigateTo = () => history.push(HOME_PAGE);
+
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>404 Not Found 🙁</h1>
-    </div>
+    <VStack color="blue.300" height="100vh" justifyContent="center" spacing={8}>
+      <Text textStyle="subtitle1">Error 404</Text>
+      <Text textStyle="header4">You are not authorized to view this page.</Text>
+      <Button variant="primary" my={2} onClick={navigateTo}>
+        Go to home page
+      </Button>
+    </VStack>
   );
 };
 
