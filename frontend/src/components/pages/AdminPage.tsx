4ff0a304176f3d7471832ac0d259be8f554799d4
victoranyun <victoryun1811@gmail.com>
2022-11-17 14:04:30 -0500

    feat: admin page

--- frontend/src/components/pages/AdminPage.tsx
+++ frontend/src/components/pages/AdminPage.tsx
@@ -1,35 +1,115 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import * as Routes from "../../constants/Routes";
+import {
+  Flex,
+  Text,
+  Box,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import SideBar from "../common/Sidebar";
 
-import Logout from "../auth/Logout";
-import RefreshCredentials from "../auth/RefreshCredentials";
-import ResetPassword from "../auth/ResetPassword";
+import Page from "../../types/PageTypes";
+import AdminUserTable from "../common/AdminUserTable";
+import AddAdminModal from "../common/AddAdminModal";
+import { SettingsOutlineIcon } from "../common/icons";
 
-type ButtonProps = { text: string; path: string };
+const pages: Page[] = [
+  { title: "Assessments", url: "/", icon: SettingsOutlineIcon },
+  { title: "Database", url: "/", icon: SettingsOutlineIcon },
+];
 
-const Button = ({ text, path }: ButtonProps) => {
-  const history = useHistory();
-  const navigateTo = () => history.push(path);
-  return (
-    <button className="btn btn-primary" onClick={navigateTo} type="button">
-      {text}
-    </button>
-  );
-};
+const USERS = [
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email:
+      "The quick brown fox jumps over the lazy dog is an English-language pangram—a sentence that contains all of the letters of the English alphabet. Owing to its existence, Chakra was created.",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+  {
+    firstName: "Albert",
+    lastName: "Kuhl",
+    email: "albertkuhl@jumpmath.ca",
+  },
+];
 
 const AdminPage = (): React.ReactElement => {
+  const unselectedColor = useColorModeValue("#727278", "#727278");
   return (
-    <div style={{ textAlign: "center", margin: "0px auto" }}>
-      <h1>Admin Page</h1>
-      <div className="btn-group">
-        <Logout />
-        <RefreshCredentials />
-        <ResetPassword />
-        <Button text="Student Page" path={Routes.STUDENT_PAGE} />
-        <Button text="Component Library" path={Routes.COMPONENT_LIBRARY} />
-      </div>
-    </div>
+    <Flex mx="4">
+      <Box p={0} w="20%">
+        <SideBar pages={pages} />
+      </Box>
+      <Flex direction="column" w="100%" pt={20}>
+        <Text
+          textStyle="header4"
+          color="blue.300"
+          style={{ textAlign: "left" }}
+        >
+          Database
+        </Text>
+        <Box textAlign="right" pr={10}>
+          <AddAdminModal />
+        </Box>
+        <Tabs pr={5}>
+          <TabList>
+            <Tab color={unselectedColor}>Admin</Tab>
+            <Tab color={unselectedColor}>Teachers</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <AdminUserTable adminUsers={USERS} />
+            </TabPanel>
+            <TabPanel>
+              <AdminUserTable adminUsers={USERS} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
+    </Flex>
   );
 };
 
