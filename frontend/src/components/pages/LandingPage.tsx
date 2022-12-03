cbfc352fe70e3cedbae82bcea6a11208b731cec8
joyce-shi <j224shi@git.uwaterloo.ca>
2022-12-03 00:51:15 -0500

    finishing cosmetic updates

--- frontend/src/components/pages/LandingPage.tsx
+++ frontend/src/components/pages/LandingPage.tsx
@@ -1,47 +1,60 @@
 import React from "react";
 import {
   Box,
+  Center,
   Text,
+  Stack,
   VStack,
   Image,
   Button,
-  ButtonGroup,
+  Link,
 } from "@chakra-ui/react";
 
-import RouterLink from "../common/RouterLink";
 import JumpMathLogo from "../../assets/jump-math-logo.png";
+
+const NavOptions = (): React.ReactElement => (
+  <VStack gap={1.5}>
+    <Button width={300} variant="primary">
+      Admin
+    </Button>
+    <Button width={300} variant="primary">
+      Teacher
+    </Button>
+    <Button width={300} variant="primary">
+      Student
+    </Button>
+  </VStack>
+);
 
 const LandingPage = (): React.ReactElement => {
   return (
-    <VStack gap={3}>
-      <RouterLink to="jumpmath.org">Back to Home</RouterLink>
-      <Box
-        p={10}
-        width="80%"
-        borderColor="grey.200"
-        borderWidth="1px"
-        borderRadius="12px"
-      >
-        <Image src={JumpMathLogo} alt="Jump Math Logo" h="100px" />
-        <Text textStyle="header4" textAlign="center">
-          Welcome to the Online Assessment Platform
-        </Text>
-        <Text textStyle="subtitle1" textAlign="center">
-          To begin, are you a...
-        </Text>
-        <VStack gap={1.5}>
-          <Button width={300} variant="primary">
-            Admin
-          </Button>
-          <Button width={300} variant="primary">
-            Teacher
-          </Button>
-          <Button width={300} variant="primary">
-            Student
-          </Button>
-        </VStack>
-      </Box>
-    </VStack>
+    <Box>
+      <Stack p={8} pb={20} justifyContent="left">
+        <Link textStyle="subtitle2" href="https://jumpmath.org/">
+          {"< Back to Home"}
+        </Link>
+      </Stack>
+      <Center>
+        <Box
+          p={10}
+          width="60%"
+          borderColor="grey.200"
+          borderWidth="1px"
+          borderRadius="12px"
+        >
+          <VStack gap={6}>
+            <Image src={JumpMathLogo} alt="Jump Math Logo" h={90} />
+            <Text textStyle="header4" textAlign="center">
+              Welcome to the Online Assessment Platform
+            </Text>
+            <Text textStyle="subtitle1" textAlign="center">
+              To begin, are you a...
+            </Text>
+            <NavOptions />
+          </VStack>
+        </Box>
+      </Center>
+    </Box>
   );
 };
 
