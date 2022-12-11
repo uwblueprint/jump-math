a27282be0f09fe8825326de47f5727455a7fae1b
Boya <boyazhang123@gmail.com>
2022-12-11 17:42:07 -0500

    feat: create admin login page frontend

--- frontend/src/assets/icons.tsx
+++ frontend/src/assets/icons.tsx
@@ -239,6 +239,15 @@ export const CheckmarkCircleFillIcon = (): React.ReactElement => (
     <path
       fill="currentColor"
       d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm4.3 7.61l-4.57 6a1 1 0 0 1-.79.39 1 1 0 0 1-.79-.38l-2.44-3.11a1 1 0 0 1 1.58-1.23l1.63 2.08 3.78-5a1 1 0 1 1 1.6 1.22z"
+      />
+  </Icon>
+);
+
+export const AlertCircle = (): React.ReactElement => (
+  <Icon color="red.200" boxSize="20px">
+    <path
+      fill="currentColor"
+      d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 15a1 1 0 1 1 1-1 1 1 0 0 1-1 1zm1-4a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0z"
     />
   </Icon>
 );
--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,26 +1,48 @@
 import React, { useContext, useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { useMutation } from "@apollo/client";
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Button,
+} from "@chakra-ui/react";
+
 import authAPIClient from "../../APIClients/AuthAPIClient";
 import { LOGIN } from "../../APIClients/mutations/AuthMutations";
 import { HOME_PAGE, SIGNUP_PAGE } from "../../constants/Routes";
 import AuthContext from "../../contexts/AuthContext";
 import { AuthenticatedUser } from "../../types/AuthTypes";
+import JumpMathLogo from "../../assets/jump-math-logo.png";
+import KidsPicture from "../../assets/group-kids.png";
+import { AlertCircle } from "../../assets/icons";
 
 const Login = (): React.ReactElement => {
   const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+
   const history = useHistory();
 
   const [login] = useMutation<{ login: AuthenticatedUser }>(LOGIN);
 
   const onLogInClick = async () => {
+    if (!(email && password)) {
+      setLoginError("Please ensure fields are filled");
+      return;
+    }
     const user: AuthenticatedUser = await authAPIClient.login(
       email,
       password,
       login,
     );
+
     setAuthenticatedUser(user);
   };
 
@@ -33,45 +55,87 @@ const Login = (): React.ReactElement => {
   }
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="username@domain.com"
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder="password"
-          />
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={onLogInClick}
-          >
-            Log In
-          </button>
-        </div>
-      </form>
-      <div>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={onSignUpClick}
+    <Flex h="100vh">
+      <Box w="50%">
+        <Image src={KidsPicture} />
+      </Box>
+      <Flex w="50%" justifyContent="center">
+        <Flex
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          maxWidth="450px"
         >
-          Sign Up
-        </button>
-      </div>
-    </div>
+          <Image src={JumpMathLogo} alt="Jump Math Logo" w="296px" mb="24px" />
+          <Text textStyle="header4" mb="24px">
+            Admin Login
+          </Text>
+          <Text textStyle="subtitle2" mb={loginError ? "12px" : "48px"}>
+            Enter your login credentials below to continue
+          </Text>
+          {loginError && (
+            <Flex alignItems="center" pb="12px">
+              <AlertCircle />{" "}
+              <Text color="red.200" pl="5px">
+                Please ensure fields are filled
+              </Text>
+            </Flex>
+          )}
+          <FormControl textStyle="subtitle2" isRequired>
+            <Flex>
+              <Text textStyle="subtitle2">Email Address</Text>
+              <FormLabel />
+            </Flex>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              mb="24px"
+              placeholder="Enter Jump Math email address"
+              variant="filled"
+            />
+            <Flex>
+              <Text textStyle="subtitle2">Password</Text>
+              <FormLabel />
+            </Flex>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              mb="32px"
+              placeholder="Enter Password"
+              variant="filled"
+            />
+          </FormControl>
+          <Checkbox
+            colorScheme="gray"
+            defaultChecked
+            mb="24px"
+            alignSelf="start"
+          >
+            <Text textStyle="paragraph">Remember me</Text>
+          </Checkbox>
+          <Button onClick={onLogInClick} variant="primary" w="full" mb="24px">
+            Login
+          </Button>
+          {loginError ? (
+            <Text onClick={onSignUpClick}>
+              Don&apos;t have an account?{" "}
+              <Text
+                as="u"
+                onClick={onSignUpClick}
+                color="blue.300"
+                cursor="pointer"
+              >
+                Sign Up
+              </Text>
+            </Text>
+          ) : (
+            <Text color="blue.300">Forgot Password?</Text>
+          )}
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
 
