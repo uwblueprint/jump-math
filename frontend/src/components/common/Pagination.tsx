80e3f4393cc10cc18153014880fb0f8ecf097093
Nima Sheth <nimasheth@Nimas-MacBook-Air.local>
2023-02-09 22:31:20 -0500

    other changes

--- frontend/src/components/common/Pagination.tsx
+++ frontend/src/components/common/Pagination.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { chakra, ChakraProvider } from "@chakra-ui/react";
 import {
   Pagination as P,
   usePagination,
@@ -10,6 +10,20 @@ import {
   PaginationPageGroup,
 } from "@ajna/pagination";
 
+const customTheme = {
+  ...chakra,
+  fonts: {
+    body: '"DM Sans"',
+    heading: '"DM Sans"',
+    mono: '"DM Sans"',
+  },
+  colors: {
+    brand: {
+      900: "#154472",
+    },
+  },
+};
+
 const Pagination = (): React.ReactElement => {
   const { currentPage, setCurrentPage, pagesCount, pages } = usePagination({
     pagesCount: 6,
@@ -17,7 +31,7 @@ const Pagination = (): React.ReactElement => {
   });
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <P
         pagesCount={pagesCount}
         currentPage={currentPage}
@@ -32,6 +46,9 @@ const Pagination = (): React.ReactElement => {
               border="none"
               textDecoration="underline"
               _hover={{ textDecoration: "underline", fontWeight: "600" }}
+              style={{
+                margin: "0 10px",
+              }}
               outline="none"
             >
               &lt; Previous
@@ -41,7 +58,6 @@ const Pagination = (): React.ReactElement => {
           <PaginationPageGroup>
             {pages.map((page: number) => (
               <PaginationPage
-                w={10}
                 bg="#E8EDF1"
                 key={`pagination_page_${page}`}
                 page={page}
@@ -53,11 +69,18 @@ const Pagination = (): React.ReactElement => {
                 _current={{
                   bg: "#A1B4C7",
                   fontFamily: "DM Sans",
-                  w: 10,
                   borderRadius: "50%",
                   color: "#F4F4F4",
                 }}
-                style={{ borderRadius: "50%" }}
+                style={{
+                  height: "2.25rem",
+                  width: "2.25rem",
+                  borderRadius: "50%",
+                  textAlign: "center",
+                  lineHeight: "2rem",
+                  display: "inline-block",
+                  margin: "0 2px",
+                }}
               />
             ))}
           </PaginationPageGroup>
@@ -69,6 +92,9 @@ const Pagination = (): React.ReactElement => {
               border="none"
               textDecoration="underline"
               _hover={{ textDecoration: "underline", fontWeight: "600" }}
+              style={{
+                margin: "0 10px",
+              }}
             >
               Next &gt;
             </PaginationNext>
