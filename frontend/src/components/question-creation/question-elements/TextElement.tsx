5c8ce044b8af5c5ae60d64c1eee845b76f4c8474
joyce-shi <j224shi@git.uwaterloo.ca>
2023-02-20 16:35:46 -0500

    style text element component

--- frontend/src/components/question-creation/question-elements/TextElement.tsx
+++ frontend/src/components/question-creation/question-elements/TextElement.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-import { HStack, Input } from "@chakra-ui/react";
+import { HStack, Input, Box } from "@chakra-ui/react";
 import { DeleteOutlineIcon, HamburgerMenuIcon } from "../../../assets/icons";
 
 const TextElement = (): React.ReactElement => {
   const [text, setText] = useState("");
 
   return (
-    <HStack>
-      <HamburgerMenuIcon />
+    <HStack spacing="6">
+      <Box color="grey.300">
+        <HamburgerMenuIcon />
+      </Box>
       <Input
+        maxLength={800}
         value={text}
         onChange={(e) => setText(e.target.value)}
-        placeholder="This is a text component."
+        placeholder="Welcome to the question creation module."
+        variant="unstyled"
       />
-      <DeleteOutlineIcon />
+      <Box color="grey.300">
+        <DeleteOutlineIcon />
+      </Box>
     </HStack>
   );
 };
