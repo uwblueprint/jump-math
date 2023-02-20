9949966b070c4f04047354462f577e303de3c5f5
Nima Sheth <60047036+Nimasheth@users.noreply.github.com>
2023-02-19 21:22:13 -0500

    feat: Pagination on UserPage (#120)
    * pagination
    
    * pagination
    
    * changes
    
    * other changes
    
    * pagination
    
    * pagination
    
    * finished
    
    * changes
    
    * DONE
    
    * error fixed
    
    * changed itemsperpage
    
    * made seperate file for components
    
    * changes
    
    * finished pagination
    
    * adding PaginationStyle to themes
    
    * delete paginationts
    
    * removed prop-types
    
    * pagination
    
    * final changes
    
    * changes
    
    * changes
    
    * final changes
    
    ---------
    
    Co-authored-by: Nima Sheth <nimasheth@Nimas-MacBook-Air.local>
    Co-authored-by: Nima Sheth <nimasheth@v1040-wn-rt-b-3-160.campus-dynamic.uwaterloo.ca>
    Co-authored-by: Nima Sheth <nimasheth@v1040-wn-rt-a-126-146.campus-dynamic.uwaterloo.ca>

---
+++ frontend/src/components/common/Pagination.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import {
+  Pagination as P,
+  usePagination,
+  PaginationNext,
+  PaginationPage,
+  PaginationPrevious,
+  PaginationContainer,
+  PaginationPageGroup,
+  PaginationSeparator,
+} from "@ajna/pagination";
+
+const outerLimit = 1;
+const innerLimit = 1;
+
+interface PaginationProps {
+  pagesCount: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination = ({
+  pagesCount,
+  currentPage,
+  onPageChange,
+}: PaginationProps): React.ReactElement => {
+  const { pages } = usePagination({
+    pagesCount,
+    initialState: { currentPage },
+  });
+
+  return (
+    <P
+      pagesCount={pagesCount}
+      currentPage={currentPage}
+      onPageChange={onPageChange}
+    >
+      <PaginationContainer>
+        {currentPage !== 1 && (
+          <PaginationPrevious
+            size="paginationNavigate"
+            variant="paginationNavigate"
+          >
+            &lt; Previous
+          </PaginationPrevious>
+        )}
+        <PaginationPageGroup isInline align="center">
+          {pages.map((page: number, index: number) => {
+            if (
+              index === 0 ||
+              index === pages.length - 1 ||
+              (index >= currentPage - innerLimit - 1 &&
+                index <= currentPage + innerLimit - 1)
+            ) {
+              return (
+                <PaginationPage
+                  size="paginationStyle"
+                  variant="paginationStyle"
+                  key={`pagination_page_${page}`}
+                  page={page}
+                  _current={{
+                    bg: "blue.100",
+                    color: "grey.100",
+                  }}
+                />
+              );
+            }
+            if (
+              (index === currentPage - innerLimit - 2 &&
+                currentPage > outerLimit + innerLimit + 1) ||
+              (index === currentPage + innerLimit &&
+                currentPage < pagesCount - outerLimit - innerLimit)
+            ) {
+              return (
+                <PaginationSeparator
+                  size="paginationStyle"
+                  key={`pagination_ellipsis_${index}`}
+                  sx={{
+                    opacity: "1 !important",
+                    bg: "blue.50",
+                    color: "grey.300",
+                  }}
+                />
+              );
+            }
+            return null;
+          })}
+        </PaginationPageGroup>
+
+        {currentPage !== pagesCount && (
+          <PaginationNext
+            size="paginationNavigate"
+            variant="paginationNavigate"
+          >
+            Next &gt;
+          </PaginationNext>
+        )}
+      </PaginationContainer>
+    </P>
+  );
+};
+
+export default Pagination;
