6e093812a2bb2330e8fed9b6f5f1555fa5608846
joyce-shi <j224shi@git.uwaterloo.ca>
2023-02-21 16:51:01 -0500

    fix multiline text issue

--- frontend/src/components/question-creation/question-elements/TextElement.tsx
+++ frontend/src/components/question-creation/question-elements/TextElement.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Input } from "@chakra-ui/react";
+import { Textarea } from "@chakra-ui/react";
+import ResizeTextarea from "react-textarea-autosize";
 import QuestionElement from "../QuestionElement";
 
 const TextElement = (): React.ReactElement => {
@@ -7,12 +8,17 @@ const TextElement = (): React.ReactElement => {
 
   return (
     <QuestionElement>
-      <Input
+      <Textarea
         maxLength={800}
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="This is a text component which can be added for any additional information."
         variant="unstyled"
+        minH="unset"
+        overflow="hidden"
+        resize="none"
+        minRows={1}
+        as={ResizeTextarea}
       />
     </QuestionElement>
   );
