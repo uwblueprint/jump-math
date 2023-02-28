8c8f12266673959d971e97c495689f6431239c8b
joyce-shi <j224shi@git.uwaterloo.ca>
2023-02-27 19:16:48 -0500

    initial short answer modal

--- frontend/src/components/question-creation/question-elements/ShortAnswerElementModal.tsx
+++ frontend/src/components/question-creation/question-elements/ShortAnswerElementModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   Button,
   Modal,
@@ -6,25 +6,76 @@ import {
   ModalContent,
   ModalFooter,
   useDisclosure,
+  ModalHeader,
+  ModalCloseButton,
+  Text,
+  Divider,
+  ModalBody,
+  FormLabel,
+  FormControl,
+  Input,
 } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
+
 import ModalText from "../../common/ModalText";
 import QuestionEditorContext from "../../../contexts/QuestionEditorContext";
+import QuestionEditor from "../QuestionEditor";
+import { QuestionElementType } from "../../../types/QuestionTypes";
 
 const ShortAnswerElementModal = (): React.ReactElement => {
-  const { showShortAnswerModal } = useContext(QuestionEditorContext);
-  const { onOpen, onClose } = useDisclosure();
+  const { onClose } = useDisclosure();
+  const { setShowShortAnswerModal, showShortAnswerModal } = useContext(
+    QuestionEditorContext,
+  );
+  const closeModal = () => {
+    setShowShortAnswerModal(false);
+    onClose();
+  };
+
+  const { setQuestionElements } = useContext(QuestionEditorContext);
+  const addShortAnswerElement = (data: string) => {
+    setQuestionElements((prevElements) => [
+      ...prevElements,
+      {
+        id: uuidv4(),
+        type: QuestionElementType.SHORT_ANSWER,
+        data,
+      },
+    ]);
+  };
+
+  const [answer, setAnswer] = useState("");
+
+  const handleSubmit = () => {
+    addShortAnswerElement(answer);
+    closeModal();
+  };
+
   return (
     <>
-      <Modal isOpen={showShortAnswerModal} onClose={onClose} isCentered>
+      <Modal isOpen={showShortAnswerModal} onClose={closeModal} isCentered>
         <ModalOverlay />
-        <ModalContent alignItems="center" p={2} maxW="600px">
-          <ModalText
-            header="Unable to remove user at this moment. Please try again."
-            textColor="red.200"
-          />
-          <ModalFooter my={3}>
-            <Button variant="primary" mr={2}>
-              Return to Database
+        <ModalContent>
+          <ModalHeader>
+            <Text textStyle="subtitle2">Create multi-select question</Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl isRequired>
+              <FormLabel color="grey.300">Enter correct answer</FormLabel>
+              <Input
+                placeholder="Input Field"
+                onChange={(e) => setAnswer(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+          <Divider color="grey.200" style={{ marginTop: "1.5em" }} />
+          <ModalFooter>
+            <Button variant="secondary" onClick={closeModal}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleSubmit}>
+              Confirm
             </Button>
           </ModalFooter>
         </ModalContent>
