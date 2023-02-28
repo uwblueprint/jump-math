4c927b2dac00ed904de4082972c71b809bc6f107
wais-hundekar <wais.hundekar@gmail.com>
2023-02-27 22:10:53 -0500

    name changes

--- frontend/src/components/auth/StudentLogin/StudentNameSelection.tsx
+++ frontend/src/components/auth/StudentLogin/StudentNameSelection.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import {
   FormControl,
   FormLabel,
-  Button,
   FormErrorMessage,
+  Box,
 } from "@chakra-ui/react";
 import { Select } from "chakra-react-select";
 
@@ -11,8 +11,8 @@ import { Controller, useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import AuthWrapper from "../AuthWrapper";
 import { STUDENT_SIGNUP_IMAGE } from "../../../assets/images";
-import { LeftArrowIcon } from "../../../assets/icons";
 import * as Routes from "../../../constants/Routes";
+import NavigationButtons from "../TeacherSignup/NavigationButtons";
 
 const students = [
   {
@@ -59,43 +59,39 @@ const StudentNameSelection = (): React.ReactElement => {
   const subtitle = "Please enter your name in the field or search for it below";
   const image = STUDENT_SIGNUP_IMAGE;
   const form = (
-    <Controller
-      control={control}
-      name="grade"
-      rules={{ required: "Please select a grade" }}
-      render={({ field: { onChange, value, name }, fieldState: { error } }) => (
-        <FormControl isRequired isInvalid={Boolean(error)}>
-          <FormLabel color="grey.400">First Name</FormLabel>
-          <Select
-            name={name}
-            onChange={onChange}
-            options={students}
-            value={value}
-            placeholder="Search Name by typing it in field"
-            useBasicStyles
-          />
-          <Button variant="primary" mt="10%" width="100%">
-            Continue
-          </Button>
-          <Button
-            onClick={() => {
-              history.push(Routes.HOME_PAGE);
-            }}
-            mt="10%"
-            width="100%"
-            leftIcon={<LeftArrowIcon />}
-            textStyle="paragraph"
-            fontWeight="500"
-            variant="link"
-            _hover={{ color: "blue.200", textDecoration: "none" }}
-            color="blue.300"
-          >
-            Back to Home
-          </Button>
-          <FormErrorMessage>{error?.message}</FormErrorMessage>
-        </FormControl>
-      )}
-    />
+    <>
+      <Box width="100%" pb="10%">
+        <Controller
+          control={control}
+          name="grade"
+          rules={{ required: "Please select a grade" }}
+          render={({
+            field: { onChange, value, name },
+            fieldState: { error },
+          }) => (
+            <FormControl isRequired isInvalid={Boolean(error)}>
+              <FormLabel color="grey.400">First Name</FormLabel>
+              <Select
+                name={name}
+                onChange={onChange}
+                options={students}
+                value={value}
+                placeholder="Search Name by typing it in field"
+                useBasicStyles
+              />
+              <FormErrorMessage>{error?.message}</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+      </Box>
+      <NavigationButtons
+        onContinueClick={() => undefined}
+        onBackClick={() => {
+          history.push(Routes.HOME_PAGE);
+        }}
+        backButtonText="Back to Home"
+      />
+    </>
   );
 
   return (
