100be153729a2cc1e32de8068bacf3ab451fa844
joyce-shi <j224shi@git.uwaterloo.ca>
2023-03-01 21:18:32 -0500

    cosmetic fixes

--- frontend/src/components/question-creation/question-elements/ShortAnswerElement.tsx
+++ frontend/src/components/question-creation/question-elements/ShortAnswerElement.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Input } from "@chakra-ui/react";
+import React from "react";
+import { Input, Box } from "@chakra-ui/react";
 
 interface ShortAnswerElementProps {
   id: string;
@@ -11,14 +11,17 @@ const ShortAnswerElement = ({
   data,
 }: ShortAnswerElementProps): React.ReactElement => {
   return (
-    <Input
-      value={data}
-      readOnly
-      variant="outline"
-      borderRadius="0"
-      borderColor="grey.300"
-      focusBorderColor="grey.300"
-    />
+    <Box width="100%" paddingLeft="6">
+      <Input
+        value={data}
+        readOnly
+        variant="outline"
+        borderRadius="0"
+        borderColor="grey.300"
+        focusBorderColor="grey.300"
+        width="34%"
+      />
+    </Box>
   );
 };
 
