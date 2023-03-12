5ced7933fe0b640115e40c25a263d25a8c7f293b
Jenna Xiao <jenna_xiao@hotmail.com>
2023-03-12 19:29:00 -0400

    resolved pr comments

--- backend/graphql/resolvers/testSessionResolvers.ts
+++ backend/graphql/resolvers/testSessionResolvers.ts
@@ -1,9 +1,8 @@
-import { GraphQLScalarType, Kind } from "graphql";
+import { GraphQLScalarType } from "graphql";
 import TestSessionService from "../../services/implementations/testSessionService";
 import UserService from "../../services/implementations/userService";
 import TestService from "../../services/implementations/testService";
 import SchoolService from "../../services/implementations/schoolService";
-
 import {
   ITestSessionService,
   TestSessionRequestDTO,
@@ -12,6 +11,10 @@ import {
 import { ITestService } from "../../services/interfaces/testService";
 import IUserService from "../../services/interfaces/userService";
 import { ISchoolService } from "../../services/interfaces/schoolService";
+import {
+  validatePrimitive,
+  validateArray,
+} from "../../middlewares/validators/util";
 
 const userService: IUserService = new UserService();
 const schoolService: ISchoolService = new SchoolService(userService);
@@ -28,34 +31,33 @@ const testSessionResolvers = {
     description: "A Number or An Array or Null",
     serialize(value) {
       if (
-        typeof value === "number" ||
         typeof value === null ||
-        (Array.isArray(value) &&
-          value.every(function (e) {
-            return typeof e === "number";
-          }))
+        validatePrimitive(value, "number") ||
+        validateArray(value, "number")
       ) {
         return value;
       }
-      throw new Error("Answers must be either a Number or Array or Null");
+      throw new Error(
+        "The 'answers' field under 'results' must be an array containing only numbers, number arrays, and/or nulls.",
+      );
     },
     // parseValue and parseLiteral will need to be created for creating test sessions mutation
   }),
   Query: {
-    testSessionByAccessCode: async (
-      _parent: undefined,
-      { accessCode }: { accessCode: string },
-    ): Promise<TestSessionResponseDTO> => {
-      return testSessionService.getTestSessionByAccessCode(accessCode);
-    },
-    testSessions: async (): Promise<TestSessionResponseDTO[]> => {
-      return testSessionService.getAllTestSessions();
-    },
     testSession: async (
       _parent: undefined,
       { id }: { id: string },
     ): Promise<TestSessionResponseDTO> => {
       return testSessionService.getTestSessionById(id);
+    },
+    testSessions: async (): Promise<TestSessionResponseDTO[]> => {
+      return testSessionService.getAllTestSessions();
+    },
+    testSessionByAccessCode: async (
+      _parent: undefined,
+      { accessCode }: { accessCode: string },
+    ): Promise<TestSessionResponseDTO> => {
+      return testSessionService.getTestSessionByAccessCode(accessCode);
     },
   },
   Mutation: {
