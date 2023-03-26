523ec7ef39b031eba8d43d5ed7e3110ea473ae7a
joyce-shi <j224shi@git.uwaterloo.ca>
2023-03-26 18:56:28 -0400

    fixes

--- frontend/src/components/assessments/assessment-creation/QuestionSummary.tsx
+++ frontend/src/components/assessments/assessment-creation/QuestionSummary.tsx
@@ -17,7 +17,6 @@ const QuestionSummary = ({
       backgroundColor="rgba(232, 237, 241, 0.2)"
       borderRadius="16px"
       padding="8"
-      width="33%"
     >
       <VStack align="left" spacing="2">
         <Text color="blue.300" paddingBottom="7" textStyle="subtitle2">
