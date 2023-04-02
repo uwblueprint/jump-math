b403cb6c7c920f3912425d5200817d5d5c4c062c
wais-hundekar <wais.hundekar@gmail.com>
2023-04-02 00:13:30 -0400

    create student resolver

---
+++ backend/graphql/resolvers/classResolvers.ts
@@ -0,0 +1,41 @@
+import ClassService from "../../services/implementations/classService";
+import SchoolService from "../../services/implementations/schoolService";
+import TestService from "../../services/implementations/testService";
+import TestSessionService from "../../services/implementations/testSessionService";
+import UserService from "../../services/implementations/userService";
+import {
+  ClassResponseDTO,
+  IClassService,
+  StudentRequestDTO,
+} from "../../services/interfaces/classService";
+import { ISchoolService } from "../../services/interfaces/schoolService";
+import { ITestService } from "../../services/interfaces/testService";
+import { ITestSessionService } from "../../services/interfaces/testSessionService";
+import IUserService from "../../services/interfaces/userService";
+
+const userService: IUserService = new UserService();
+const testService: ITestService = new TestService();
+const schoolService: ISchoolService = new SchoolService(userService);
+const testSessionService: ITestSessionService = new TestSessionService(
+  testService,
+  userService,
+  schoolService,
+);
+const classService: IClassService = new ClassService(
+  userService,
+  testSessionService,
+);
+
+const classResolvers = {
+  Mutation: {
+    createStudent: async (
+      _req: undefined,
+      { student }: { student: StudentRequestDTO },
+      classId: string,
+    ): Promise<ClassResponseDTO> => {
+      return classService.createStudent(student, classId);
+    },
+  },
+};
+
+export default classResolvers;
