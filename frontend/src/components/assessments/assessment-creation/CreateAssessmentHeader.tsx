9986a240e3c7afa05da7fd45e415b68b2a8557dd
joyce-shi <joyce.shi@uwaterloo.ca>
2023-04-05 18:40:38 -0400

    show publish modal

--- frontend/src/components/assessments/assessment-creation/CreateAssessmentHeader.tsx
+++ frontend/src/components/assessments/assessment-creation/CreateAssessmentHeader.tsx
@@ -16,11 +16,12 @@ import {
 } from "../../../assets/icons";
 import { getReadableDate } from "../../../utils/GeneralUtils";
 import BackButton from "../../common/BackButton";
+import PublishModal from "../assessment-status/EditStatusModals/PublishModal";
 
 interface CreateAssessementHeaderProps {
   name: string;
-  onSave: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  onPublish: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onSave: () => void;
+  onPublish: () => void;
 }
 
 const CreateAssessementHeader = ({
@@ -28,47 +29,55 @@ const CreateAssessementHeader = ({
   onSave,
   onPublish,
 }: CreateAssessementHeaderProps): React.ReactElement => {
+  const [showPublishModal, setShowPublishModal] = React.useState(false);
   return (
-    <Box
-      borderBottom="1px"
-      borderBottomColor="grey.200"
-      padding="1.5em 2em 1.5em 2em"
-      width="100%"
-    >
-      <Flex minWidth="max-content">
-        <HStack alignItems="start" spacing={6}>
-          <BackButton />
-          <VStack align="left">
-            <Text textStyle="subtitle1">{name || "Untitled Assessment"}</Text>
-            <Text textStyle="smallerParagraph">
-              Created {getReadableDate()}
-            </Text>
-          </VStack>
-        </HStack>
-        <Spacer />
-        <HStack spacing={2}>
-          <Button leftIcon={<EyeOutlineIcon />} size="sm" variant="tertiary">
-            Preview
-          </Button>
-          <Button
-            leftIcon={<SaveOutlineIcon />}
-            minWidth="10"
-            onClick={onSave}
-            variant="secondary"
-          >
-            Save
-          </Button>
-          <Button
-            leftIcon={<TextOutlineIcon />}
-            minWidth="10"
-            onClick={onPublish}
-            variant="primary"
-          >
-            Publish
-          </Button>
-        </HStack>
-      </Flex>
-    </Box>
+    <>
+      <Box
+        borderBottom="1px"
+        borderBottomColor="grey.200"
+        padding="1.5em 2em 1.5em 2em"
+        width="100%"
+      >
+        <Flex minWidth="max-content">
+          <HStack alignItems="start" spacing={6}>
+            <BackButton />
+            <VStack align="left">
+              <Text textStyle="subtitle1">{name || "Untitled Assessment"}</Text>
+              <Text textStyle="smallerParagraph">
+                Created {getReadableDate()}
+              </Text>
+            </VStack>
+          </HStack>
+          <Spacer />
+          <HStack spacing={2}>
+            <Button leftIcon={<EyeOutlineIcon />} size="sm" variant="tertiary">
+              Preview
+            </Button>
+            <Button
+              leftIcon={<SaveOutlineIcon />}
+              minWidth="10"
+              onClick={onSave}
+              variant="secondary"
+            >
+              Save
+            </Button>
+            <Button
+              leftIcon={<TextOutlineIcon />}
+              minWidth="10"
+              onClick={() => setShowPublishModal(true)}
+              variant="primary"
+            >
+              Publish
+            </Button>
+          </HStack>
+        </Flex>
+      </Box>
+      <PublishModal
+        isOpen={showPublishModal}
+        onClose={() => setShowPublishModal(false)}
+        publishAssessment={onPublish}
+      />
+    </>
   );
 };
 
