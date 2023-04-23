61185b5cc0372ff79bfb165e1ba89dce2d22ef53
Jenna Xiao <jenna_xiao@hotmail.com>
2023-04-23 15:17:47 -0400

    created TestSubmissionMessage

--- frontend/src/components/pages/ComponentLibrary.tsx
+++ frontend/src/components/pages/ComponentLibrary.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-import QuestionNumberTypes from "../../types/QuestionNumberTypes";
-import QuestionNumber from "../assessments/assessment-creation/QuestionNumber";
+import TestSubmissionMessage from "../assessments/assessment-creation/TestSubmissionMessage";
 
 const ComponentLibrary = (): React.ReactElement => {
   return (
     <div>
-      <QuestionNumber num={1} status={QuestionNumberTypes.CURRENT} />
-      <br />
-      <QuestionNumber num={1} status={QuestionNumberTypes.UNATTEMPTED} />
-      <br />
-      <QuestionNumber num={1} status={QuestionNumberTypes.COMPLETED} />
+      <TestSubmissionMessage />
     </div>
   );
 };
