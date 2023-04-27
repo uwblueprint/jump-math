f57148397d9590d28391e8790df91c3ec7ef62f5
David Liu <davidliu@Davids-MacBook-Air.local>
2023-04-27 12:35:48 -0400

    init

--- backend/graphql/resolvers/userResolvers.ts
+++ backend/graphql/resolvers/userResolvers.ts
@@ -1,9 +1,17 @@
 import nodemailerConfig from "../../nodemailer.config";
 import AuthService from "../../services/implementations/authService";
+import ClassService from "../../services/implementations/classService";
 import EmailService from "../../services/implementations/emailService";
+import SchoolService from "../../services/implementations/schoolService";
+import TestService from "../../services/implementations/testService";
+import TestSessionService from "../../services/implementations/testSessionService";
 import UserService from "../../services/implementations/userService";
 import IAuthService from "../../services/interfaces/authService";
+import { IClassService } from "../../services/interfaces/classService";
 import IEmailService from "../../services/interfaces/emailService";
+import { ISchoolService } from "../../services/interfaces/schoolService";
+import { ITestService } from "../../services/interfaces/testService";
+import { ITestSessionService } from "../../services/interfaces/testSessionService";
 import IUserService from "../../services/interfaces/userService";
 import { CreateUserDTO, UpdateUserDTO, UserDTO, TeacherDTO } from "../../types";
 import { generateCSV } from "../../utilities/CSVUtils";
@@ -11,6 +19,17 @@ import { generateCSV } from "../../utilities/CSVUtils";
 const userService: IUserService = new UserService();
 const emailService: IEmailService = new EmailService(nodemailerConfig);
 const authService: IAuthService = new AuthService(userService, emailService);
+const testService: ITestService = new TestService();
+const schoolService: ISchoolService = new SchoolService(userService);
+const testSessionService: ITestSessionService = new TestSessionService(
+  testService,
+  userService,
+  schoolService,
+);
+const classService: IClassService = new ClassService(
+  userService,
+  testSessionService,
+);
 
 const userResolvers = {
   Query: {
@@ -63,13 +82,27 @@ const userResolvers = {
       _parent: undefined,
       { id }: { id: string },
     ): Promise<void> => {
-      return userService.deleteUserById(id);
+      const teacher = await userService.getUserById(id);
+      const classes = teacher.class;
+      userService.deleteUserById(id);
+      if (classes) {
+        classes.forEach((classId) => {
+          classService.deleteClass(classId);
+        });
+      }
     },
     deleteUserByEmail: async (
       _parent: undefined,
       { email }: { email: string },
     ): Promise<void> => {
-      return userService.deleteUserByEmail(email);
+      const teacher = await userService.getUserByEmail(email);
+      const classes = teacher.class;
+      userService.deleteUserByEmail(email);
+      if (classes) {
+        classes.forEach((classId) => {
+          classService.deleteClass(classId);
+        });
+      }
     },
   },
 };
