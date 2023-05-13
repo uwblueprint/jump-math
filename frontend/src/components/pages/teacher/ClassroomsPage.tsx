12d87e4c149ef1fad1ba436438380eab39a1d278
David Liu <davidliu@Davids-MacBook-Air.local>
2023-05-13 11:59:10 -0400

    added grid, pagination

--- frontend/src/components/pages/teacher/ClassroomsPage.tsx
+++ frontend/src/components/pages/teacher/ClassroomsPage.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import {
   Box,
+  Grid,
+  GridItem,
   HStack,
   Tab,
   TabList,
@@ -9,15 +11,109 @@ import {
   TabPanels,
   Tabs,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 
+import { Grade } from "../../../APIClients/types/UserClientTypes";
 import { TabEnum } from "../../../types/AuthTypes";
+import ClassroomCard from "../../classrooms/ClassroomCard";
+import Pagination from "../../common/table/Pagination";
 import AddClassroomModal from "../../user-management/student/AddClassroomModal";
 
 const ClassroomsPage = (): React.ReactElement => {
   const unselectedTabColor = "#727278";
   const [tabIndex, setTabIndex] = React.useState<TabEnum>(TabEnum.ACTIVE);
   const methods = useForm();
+  const classrooms = [
+    {
+      id: "1",
+      name: "David",
+      studentCount: 100,
+      assessmentCount: 100,
+      grade: Grade.GRADE_4,
+      activeAssessments: 100,
+    },
+    {
+      id: "2",
+      name: "David 2",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "3",
+      name: "David 3",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "4",
+      name: "David 4",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "5",
+      name: "David 5",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "6",
+      name: "David 6",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "7",
+      name: "David 7",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "8",
+      name: "David 8",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "9",
+      name: "David 9",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+    {
+      id: "10",
+      name: "David 10",
+      studentCount: 200,
+      assessmentCount: 200,
+      grade: Grade.GRADE_2,
+      activeAssessments: 200,
+    },
+  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+  const totalItems = classrooms.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
+  const itemsToShow = classrooms.slice(startIndex, endIndex);
 
   const handleTabChange = (index: TabEnum) => {
     setTabIndex(index);
@@ -46,10 +142,36 @@ const ClassroomsPage = (): React.ReactElement => {
           </TabList>
           <TabPanels>
             <TabPanel padding="0">
-              <h1>test</h1>
+              <Grid gap={4} templateColumns="repeat(5, 1fr)">
+                {itemsToShow.map((classroom) => (
+                  <GridItem key={classroom.id} flex="1" paddingTop="4">
+                    <ClassroomCard
+                      key={classroom.id}
+                      activeAssessments={classroom.activeAssessments}
+                      assessmentCount={classroom.assessmentCount}
+                      grade={classroom.grade}
+                      name={classroom.name}
+                      studentCount={classroom.studentCount}
+                    />
+                  </GridItem>
+                ))}
+              </Grid>
+              <VStack
+                alignItems="center"
+                paddingBottom="6"
+                paddingTop="6"
+                spacing="6"
+                width="100%"
+              >
+                <Pagination
+                  currentPage={currentPage}
+                  onPageChange={setCurrentPage}
+                  pagesCount={totalPages}
+                />
+              </VStack>
             </TabPanel>
             <TabPanel padding="0">
-              <h1>yert</h1>
+              <h1>Coming soon!</h1>
             </TabPanel>
           </TabPanels>
         </Tabs>
