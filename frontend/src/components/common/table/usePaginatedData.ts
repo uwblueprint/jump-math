e169db098679d5449a0630a6a6225de189b488a4
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-05-16 03:23:38 -0400

    PR feedback: type variable name

--- frontend/src/components/common/table/usePaginatedData.ts
+++ frontend/src/components/common/table/usePaginatedData.ts
@@ -1,16 +1,16 @@
 import { useMemo, useState } from "react";
 
-type PaginatedDataResult<E, Data extends Array<E> | undefined> = {
+type PaginatedDataResult<Entry, Data extends Array<Entry> | undefined> = {
   paginatedData: Data;
   currentPage: number;
   setCurrentPage: (page: number) => void;
   totalPages: number;
 };
 
-const usePaginatedData = <E, Data extends Array<E> | undefined>(
+const usePaginatedData = <Entry, Data extends Array<Entry> | undefined>(
   data: Data,
   pageSize = 8,
-): PaginatedDataResult<E, Data> => {
+): PaginatedDataResult<Entry, Data> => {
   const [currentPage, setCurrentPage] = useState(1);
 
   const numElements = data?.length ?? 0;
