005b7f7bc2a0573f7aa02f1fa373025b56c5f2bb
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2023-05-24 07:52:08 -0700

    first commit

--- backend/graphql/types/testSessionType.ts
+++ backend/graphql/types/testSessionType.ts
@@ -8,6 +8,13 @@ const testSessionType = gql`
     breakdown: [[Boolean]]!
   }
 
+  type ResultAndStudentResponseDTO {
+    student: StudentResponseDTO!
+    score: Float!
+    answers: [[[Float]]]!
+    breakdown: [[Boolean]]!
+  }
+
   input ResultRequestDTO {
     student: String!
     answers: [[[Float]]]!
@@ -26,6 +33,19 @@ const testSessionType = gql`
     notes: String
   }
 
+  type TestSessionAndStudentResponseDTO {
+    id: ID!
+    test: TestResponseDTO!
+    teacher: UserDTO!
+    school: SchoolResponseDTO!
+    class: ClassResponseDTO!
+    results: [ResultAndStudentResponseDTO]
+    accessCode: String!
+    startDate: Date!
+    endDate: Date!
+    notes: String
+  }
+
   input TestSessionRequestDTO {
     test: ID!
     teacher: ID!
@@ -38,10 +58,10 @@ const testSessionType = gql`
   }
 
   extend type Query {
-    testSession(id: String!): TestSessionResponseDTO!
+    testSession(id: ID!): TestSessionAndStudentResponseDTO!
     testSessions: [TestSessionResponseDTO]!
     testSessionByAccessCode(accessCode: String!): TestSessionResponseDTO!
-    testSessionsByTeacherId(teacherId: String!): [TestSessionResponseDTO!]!
+    testSessionsByTeacherId(teacherId: ID!): [TestSessionResponseDTO!]!
   }
 
   extend type Mutation {
