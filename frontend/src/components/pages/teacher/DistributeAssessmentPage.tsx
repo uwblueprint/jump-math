0325685e9ae40a7da88ed0c565f8570b7b2f7c27
joyce-shi <joyce.shi@uwaterloo.ca>
2023-05-26 22:23:45 -0400

    initial commit

--- frontend/src/components/pages/teacher/DistributeAssessmentPage.tsx
+++ frontend/src/components/pages/teacher/DistributeAssessmentPage.tsx
@@ -1,8 +1,59 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { HStack, Text, VStack } from "@chakra-ui/react";
+
+import type { FilterProp } from "../../common/table/FilterMenu";
+import FilterMenu from "../../common/table/FilterMenu";
+import SearchBar from "../../common/table/SearchBar";
+import type { SortOrder } from "../../common/table/SortMenu";
+import SortMenu from "../../common/table/SortMenu";
+import FormHeader from "../../sessions/distribute/FormHeader";
 
 const DistributeAssessmentPage = (): React.ReactElement => {
-  return <Text>Placeholder for Distribute Assessment page.</Text>;
+  const [search, setSearch] = useState("");
+  const [sortProperty, setSortProperty] = React.useState("updatedAt");
+  const [sortOrder, setSortOrder] = React.useState<SortOrder>("descending");
+
+  const [grades, setGrades] = React.useState<Array<string>>([]);
+  const [testTypes, setTestTypes] = React.useState<Array<string>>([]);
+  const [countries, setCountries] = React.useState<Array<string>>([]);
+  const [regions, setRegions] = React.useState<Array<string>>([]);
+
+  const [filterOptions, setFilterOptions] = React.useState<FilterProp[]>([
+    { label: "Grade", setState: setGrades, options: [] },
+    { label: "Type", setState: setTestTypes, options: [] },
+    { label: "Country", setState: setCountries, options: [] },
+    { label: "Region", setState: setRegions, options: [] },
+  ]);
+
+  return (
+    <VStack align="left">
+      <FormHeader isCurrentPage={true} />
+      <Text color="blue.300" textAlign="left" textStyle="header4">
+        Choose an Assessment
+      </Text>
+      <Text color="grey.300" textStyle="paragraph">
+        Please enter the name of the assessment you&apos;re looking for or use
+        filter/sort options to find the assessment that suits your needs.
+      </Text>
+      <HStack>
+        <SearchBar onSearch={setSearch} />
+        <SortMenu
+          initialSortOrder="descending"
+          labels={["name", "grade", "type", "country", "region"]}
+          onSortOrder={setSortOrder}
+          onSortProperty={setSortProperty}
+          properties={[
+            "name",
+            "grade",
+            "assessmentType",
+            "curriculumCountry",
+            "curriculumRegion",
+          ]}
+        />
+        <FilterMenu filterProps={filterOptions} />
+      </HStack>
+    </VStack>
+  );
 };
 
 export default DistributeAssessmentPage;
