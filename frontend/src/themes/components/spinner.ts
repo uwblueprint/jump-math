1a10dc819e851351c698fabaf3fa3a7e9f00a2d7
Joyce Shi <joyce.shi@uwaterloo.ca>
2023-05-27 23:11:26 -0400

    feat: add loading state to student login (#351)
    * add loader
    
    * add default theming to spinner

---
+++ frontend/src/themes/components/spinner.ts
@@ -0,0 +1,14 @@
+const Spinner = {
+  variants: {
+    default: {
+      color: "blue.300",
+    },
+  },
+
+  defaultProps: {
+    size: "md",
+    variant: "default",
+  },
+};
+
+export default Spinner;
