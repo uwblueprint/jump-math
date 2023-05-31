6248a3abe3712f96bb770b01c8376827b6ac9507
Joyce Shi <joyce.shi@uwaterloo.ca>
2023-05-31 14:20:18 -0400

    feat: distribute assessment flow wrapper (#352)
    * initial commit
    
    * build distribute assessment flow wrapper
    
    * add breadcrumb header
    
    * add links to breadcrumb
    
    * fix active link issue
    
    * cleanup
    
    * pr comment - top-level config

---
+++ frontend/src/components/sessions/distribute/steps/Review.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const Review = (): React.ReactElement => {
+  return (
+    <Text color="blue.300" textAlign="left" textStyle="header4">
+      Review
+    </Text>
+  );
+};
+
+export default Review;
