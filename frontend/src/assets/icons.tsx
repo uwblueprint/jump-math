697534bf4da0d2f849762d05b6c942d30889c995
Joyce Shi <joyce.shi@uwaterloo.ca>
2023-05-31 14:20:18 -0400

    feat: distribute assessment flow wrapper (#352)
    * initial commit
    
    * build distribute assessment flow wrapper
    
    * add breadcrumb header
    
    * add links to breadcrumb
    
    * fix active link issue
    
    * cleanup
    
    * pr comment - top-level config

--- frontend/src/assets/icons.tsx
+++ frontend/src/assets/icons.tsx
@@ -59,6 +59,15 @@ export const ChevronDownIcon = (): React.ReactElement => (
   </Icon>
 );
 
+export const ChevronRightIcon = (): React.ReactElement => (
+  <Icon viewBox="0 0 24 24">
+    <path
+      d="M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z"
+      fill="currentColor"
+    />
+  </Icon>
+);
+
 export const BookIcon = (props: IconProps): React.ReactElement => (
   <Icon viewBox="0 0 24 24" {...props}>
     <g clipPath="url(#clip0_46_2452)">
---
+++ frontend/src/components/sessions/distribute/steps/ChooseAssessment.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+import { Text, VStack } from "@chakra-ui/react";
+
+interface ChooseAssessmentProps {
+  setTestId: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const ChooseAssessment = ({
+  setTestId,
+}: ChooseAssessmentProps): React.ReactElement => {
+  return (
+    <VStack align="left">
+      <Text color="blue.300" textAlign="left" textStyle="header4">
+        Choose an Assessment
+      </Text>
+      <Text color="grey.300" textStyle="paragraph">
+        Please enter the name of the assessment you&apos;re looking for or use
+        filter/sort options to find the assessment that suits your needs.
+      </Text>
+    </VStack>
+  );
+};
+
+export default ChooseAssessment;
