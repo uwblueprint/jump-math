92fa1652d846362f1906b57c8d95dd2f256eefc9
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-06-26 09:12:30 -0400

    fix: remove header from test session drilldown pages (#398)
    * Remove header from drilldown page
    
    * Adjust padding on drilldown page to account for changes

--- frontend/src/components/pages/teacher/DisplayAssessmentResultsPage/DisplayAssessmentResultsByStudentPage.tsx
+++ frontend/src/components/pages/teacher/DisplayAssessmentResultsPage/DisplayAssessmentResultsByStudentPage.tsx
@@ -74,7 +74,7 @@ const DisplayAssessmentResultsByStudentPage = () => {
     studentIdToResultsMap[selectedStudentId] ?? {};
 
   return (
-    <Flex gap={14} h="calc(100vh - 350px)">
+    <Flex gap={14} h="calc(100vh - 235px)">
       <StudentList
         selectedStudentId={selectedStudentId}
         setSelectedStudentId={setSelectedStudentId}
