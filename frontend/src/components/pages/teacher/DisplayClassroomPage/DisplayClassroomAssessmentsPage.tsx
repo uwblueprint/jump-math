6a4c1c22c596ecc326b2daa2656ce2c13802e99d
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-06-28 01:50:48 -0400

    feat: add list items to test session table page

--- frontend/src/components/pages/teacher/DisplayClassroomPage/DisplayClassroomAssessmentsPage.tsx
+++ frontend/src/components/pages/teacher/DisplayClassroomPage/DisplayClassroomAssessmentsPage.tsx
@@ -1,7 +1,136 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { Center, VStack } from "@chakra-ui/react";
+
+import {
+  getSessionStatus,
+  getSessionTargetDate,
+} from "../../../../utils/TestSessionUtils";
+import EmptySessionsMessage from "../../../common/info/messages/EmptySessionsMessage";
+import Pagination from "../../../common/table/Pagination";
+import SearchableTablePage from "../../../common/table/SearchableTablePage";
+import SearchBar from "../../../common/table/SearchBar";
+import SortMenu from "../../../common/table/SortMenu";
+import usePaginatedData from "../../../common/table/usePaginatedData";
+import TestSessionListItem from "../../../teacher/view-sessions/TestSessionListItem";
+
+const mockData = {
+  sessions: [
+    ...[...Array(20)].map((_, i) => ({
+      id: i.toString(),
+      accessCode: "086731",
+      startDate: new Date("2023-05-01"),
+      endDate: new Date("2023-08-31"),
+      test: { name: "Number Recognition Assessment" },
+    })),
+    {
+      id: "20",
+      accessCode: "086731",
+      startDate: new Date("2023-04-01"),
+      endDate: new Date("2023-06-31"),
+      test: { name: "Number Recognition Assessment 2" },
+    },
+    {
+      id: "21",
+      accessCode: "123456",
+      startDate: new Date("2023-08-01"),
+      endDate: new Date("2023-08-31"),
+      test: { name: "Counting Assessment" },
+    },
+    {
+      id: "22",
+      accessCode: "123456",
+      startDate: new Date("2023-07-01"),
+      endDate: new Date("2023-07-31"),
+      test: { name: "Counting Assessment 2" },
+    },
+    {
+      id: "23",
+      accessCode: "123456",
+      stats: {
+        mean: 3.5,
+        median: 3,
+        completionRate: 30,
+        submissions: 10,
+      },
+      startDate: new Date("2021-08-01"),
+      endDate: new Date("2021-08-31"),
+      test: { name: "Assessment Name" },
+    },
+    {
+      id: "24",
+      accessCode: "123456",
+      stats: {
+        mean: 97.3,
+        median: 3,
+        completionRate: 37.1,
+        submissions: 4930,
+      },
+      startDate: new Date("2022-08-01"),
+      endDate: new Date("2022-08-31"),
+      test: { name: "Assessment Name 2" },
+    },
+  ],
+};
+
+const ASSESSEMENTS_PER_PAGE = 5;
 
 const DisplayClassroomAssessmentsPage = () => {
-  return <div>DisplayClassroomAssessmentsPage</div>;
+  const [search, setSearch] = useState("");
+
+  const data = mockData;
+
+  const formattedData = useMemo(() => {
+    const now = new Date();
+    return data?.sessions.map(
+      ({ id, startDate, endDate, test, ...session }) => ({
+        ...session,
+        testSessionId: id,
+        testName: test.name,
+        status: getSessionStatus(startDate, endDate, now),
+        targetDate: getSessionTargetDate(startDate, endDate, now),
+      }),
+    );
+  }, [data]);
+
+  const { paginatedData, totalPages, currentPage, setCurrentPage } =
+    usePaginatedData(formattedData, ASSESSEMENTS_PER_PAGE);
+
+  const tableComponent = (
+    <VStack w="100%">
+      {paginatedData?.map((session) => (
+        <TestSessionListItem key={session.testSessionId} {...session} />
+      ))}
+      {totalPages > 1 && (
+        <Center>
+          <Pagination
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+            pagesCount={totalPages}
+          />
+        </Center>
+      )}
+    </VStack>
+  );
+
+  return (
+    <SearchableTablePage
+      nameOfTableItems="assessments"
+      noResults={paginatedData.length === 0}
+      noResultsComponent={<EmptySessionsMessage />}
+      search={search}
+      searchBarComponent={<SearchBar onSearch={setSearch} />}
+      searchLength={paginatedData.length}
+      sortMenuComponent={
+        <SortMenu
+          labels={[]}
+          onSortOrder={() => {}}
+          onSortProperty={() => {}}
+          properties={[]}
+        />
+      }
+      tableComponent={tableComponent}
+    />
+  );
 };
 
 export default DisplayClassroomAssessmentsPage;
