2b14d57113bdfb872617b513377a2da2b3b02321
Joyce Shi <joyce.shi@uwaterloo.ca>
2023-07-22 21:41:27 +0200

    feat: choose classroom page (distribute assessment - page 2) (#461)
    * fix: classroom card spacing
    
    * fix: header spacing on empty state page
    
    * feat: render initial ui components
    
    * fix: minor spacing
    
    * feat: disable classroom card menu
    
    * feat: add select classroom card functionality
    
    * fix: page height
    
    * fix: parent height
    
    * pr comments: renaming prop from disabledOptions to clickDisabled
    
    * pr comments: creating distribute assessment wrapper
    
    * fix: remove unused imports
    
    * pr comments: center component theming
    
    * pr comments: file name spelling and incorrect boolean
    
    * pr comments: fix for wrong search results length

--- frontend/src/components/common/info/messages/EmptyTestsMessage.tsx
+++ frontend/src/components/common/info/messages/EmptyTestsMessage.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import { Center } from "@chakra-ui/react";
+import { Center, useStyleConfig } from "@chakra-ui/react";
 
 import DisplayAssessmentsIllustration from "../../../../assets/illustrations/display-assessments.svg";
 import { ASSESSMENT_EDITOR_PAGE } from "../../../../constants/Routes";
 import MessageContainer from "../MessageContainer";
 
 const EmptyTestsMessage = (): React.ReactElement => {
+  const styles = useStyleConfig("Center", { variant: "emptyMessage" });
   return (
-    <Center
-      backgroundColor="blue.50"
-      borderRadius="1rem"
-      color="blue.300"
-      minWidth="100%"
-      pb={14}
-    >
+    <Center __css={styles} pb={14}>
       <MessageContainer
         buttonRoute={ASSESSMENT_EDITOR_PAGE}
         buttonText="Create assessment"
