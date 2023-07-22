d4f42f353ce0325edbb1d83fc0e92e5f103560b3
wais-hundekar <87676999+wais-hundekar@users.noreply.github.com>
2023-07-22 12:37:39 -0400

    Update backend/services/implementations/__tests__/testSessionService.test.ts
    Co-authored-by: Carissa Tang <69697180+carissa-tang@users.noreply.github.com>

--- backend/services/implementations/__tests__/testSessionService.test.ts
+++ backend/services/implementations/__tests__/testSessionService.test.ts
@@ -505,6 +505,6 @@ describe("mongo testSessionService", (): void => {
 
     await expect(
       testSessionService.getTopFiveStudentsById(savedTestSession.id),
-    ).rejects.toThrowError("Test session has not ended yet");
+    ).rejects.toThrowError(`Test session has not ended yet. testSessionId: ${savedTestSession.id}`);
   });
 });
