fb19fac6e47e70f13ad62550a3bd05e5751c68ff
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-07-26 12:02:46 -0400

    Add teacher dashboard header

--- frontend/src/components/pages/teacher/TeacherDashboardPage.tsx
+++ frontend/src/components/pages/teacher/TeacherDashboardPage.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { Text } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+
+import * as Routes from "../../../constants/Routes";
+import HeaderWithButton from "../../common/HeaderWithButton";
+import SimplePopover from "../../common/popover/SimplePopover";
 
 const TeacherDashboardPage = (): React.ReactElement => {
-  return <Text>Placeholder for Teacher Dashboard page.</Text>;
+  const history = useHistory();
+
+  return (
+    <>
+      <HeaderWithButton
+        button={
+          <SimplePopover
+            items={[
+              {
+                name: "Assessment",
+                onClick: () => history.push(Routes.DISTRIBUTE_ASSESSMENT_PAGE),
+              },
+              {
+                name: "Classroom",
+                onClick: () => history.push(Routes.CLASSROOMS_PAGE),
+              },
+            ]}
+            text="Add New"
+          />
+        }
+        title="Dashboard"
+      />
+    </>
+  );
 };
 
 export default TeacherDashboardPage;
