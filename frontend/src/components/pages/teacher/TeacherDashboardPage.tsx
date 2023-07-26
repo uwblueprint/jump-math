fb499fd900a6a1a2ea2a7676419f2c94ed5b9f84
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-07-26 12:16:29 -0400

    Add dashboard sections to page

--- frontend/src/components/pages/teacher/TeacherDashboardPage.tsx
+++ frontend/src/components/pages/teacher/TeacherDashboardPage.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { HStack, Spacer, Text, VStack } from "@chakra-ui/react";
 
 import * as Routes from "../../../constants/Routes";
 import HeaderWithButton from "../../common/HeaderWithButton";
+import RouterLink from "../../common/navigation/RouterLink";
 import SimplePopover from "../../common/popover/SimplePopover";
+import AssessmentsSection from "../../teacher/dashboard/AssessmentsSection";
+import ClassroomsSection from "../../teacher/dashboard/ClassroomsSection";
 
 const TeacherDashboardPage = (): React.ReactElement => {
   const history = useHistory();
@@ -28,6 +32,42 @@ const TeacherDashboardPage = (): React.ReactElement => {
         }
         title="Dashboard"
       />
+      <HStack align="start" gap={20} mt={9}>
+        <VStack flex="1" gap={8}>
+          <HStack w="100%">
+            <Text as="h2" color="blue.300" textStyle="subtitle1">
+              Classrooms
+            </Text>
+            <Spacer />
+            <RouterLink
+              color="blue.300"
+              fontWeight="bold"
+              textDecor="none"
+              to={Routes.CLASSROOMS_PAGE}
+            >
+              View All
+            </RouterLink>
+          </HStack>
+          <ClassroomsSection />
+        </VStack>
+        <VStack align="left" flex="1" gap={0}>
+          <HStack w="100%">
+            <Text as="h2" color="blue.300" textStyle="subtitle1">
+              Assessments
+            </Text>
+            <Spacer />
+            <RouterLink
+              color="blue.300"
+              fontWeight="bold"
+              textDecor="none"
+              to={Routes.DISPLAY_ASSESSMENTS_PAGE}
+            >
+              View All
+            </RouterLink>
+          </HStack>
+          <AssessmentsSection />
+        </VStack>
+      </HStack>
     </>
   );
 };
