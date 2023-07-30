90c2828173c20a72f6ee72e8f191d2368ed80c2d
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2023-07-29 17:13:25 -0700

    refactor images to icons

--- frontend/src/constants/StudentAssessmentConstants.ts
+++ frontend/src/constants/StudentAssessmentConstants.ts
@@ -1,33 +1,33 @@
 import {
-  FRACTION,
-  MULTI_CHOICE,
-  MULTI_SELECT,
-  SHORT_ANSWER,
-} from "../assets/images";
+  FractionIcon,
+  MultipleChoiceIcon,
+  MultiSelectIcon,
+  ShortAnswerIcon,
+} from "../assets/icons";
 
 import editorTooltips from "./QuestionConstants";
 
-const typeToImageMetadata = {
+const typeToIconMetadata = {
   MULTIPLE_CHOICE: {
-    src: MULTI_CHOICE.src,
+    icon: MultipleChoiceIcon,
     alt: "multi-choice",
     tooltip: editorTooltips.MULTIPLE_CHOICE,
   },
   MULTI_SELECT: {
-    src: MULTI_SELECT.src,
+    icon: MultiSelectIcon,
     alt: "multi-select",
     tooltip: editorTooltips.MULTI_SELECT,
   },
   SHORT_ANSWER: {
-    src: SHORT_ANSWER.src,
+    icon: ShortAnswerIcon,
     alt: "short-answer",
     tooltip: editorTooltips.SHORT_ANSWER,
   },
   FRACTION: {
-    src: FRACTION.src,
+    icon: FractionIcon,
     alt: "fraction",
     tooltip: editorTooltips.FRACTION,
   },
 };
 
-export default typeToImageMetadata;
+export default typeToIconMetadata;
