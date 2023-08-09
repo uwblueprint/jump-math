961f9591e680067ab8009f9e4e7261334137ec86
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2023-08-08 23:44:14 -0700

    remove alt messages

--- frontend/src/constants/StudentAssessmentConstants.ts
+++ frontend/src/constants/StudentAssessmentConstants.ts
@@ -8,22 +8,18 @@ import {
 const typeToIconMetadata = {
   MULTIPLE_CHOICE: {
     icon: MultipleChoiceIcon,
-    alt: "multi-choice",
     tooltip: "Select the best response from a list of options",
   },
   MULTI_SELECT: {
     icon: MultiSelectIcon,
-    alt: "multi-select",
     tooltip: "Select all correct response(s) from a list of options",
   },
   SHORT_ANSWER: {
     icon: ShortAnswerIcon,
-    alt: "short-answer",
     tooltip: "Type in your answer",
   },
   FRACTION: {
     icon: FractionIcon,
-    alt: "fraction",
     tooltip: "Answer using proper, improper or mixed fractions",
   },
 };
