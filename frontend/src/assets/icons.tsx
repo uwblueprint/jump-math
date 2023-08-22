c46210c699b8980cf7095a9d5c2db8d327d1afe6
carissa-tang <69697180+carissa-tang@users.noreply.github.com>
2023-08-21 20:29:35 -0700

    update fractions icon

--- frontend/src/assets/icons.tsx
+++ frontend/src/assets/icons.tsx
@@ -399,10 +399,7 @@ export const MultiSelectTagIcon = (): React.ReactElement => (
 
 export const FractionTagIcon = (): React.ReactElement => (
   <Icon viewBox="0 0 24 24">
-    <path
-      d="M19 13H5C4.73478 13 4.48043 12.8946 4.29289 12.7071C4.10536 12.5196 4 12.2652 4 12C4 11.7348 4.10536 11.4804 4.29289 11.2929C4.48043 11.1054 4.73478 11 5 11H19C19.2652 11 19.5196 11.1054 19.7071 11.2929C19.8946 11.4804 20 11.7348 20 12C20 12.2652 19.8946 12.5196 19.7071 12.7071C19.5196 12.8946 19.2652 13 19 13Z"
-      fill="currentColor"
-    />
+    <path d="M7 21L14.9 3H17L9.1 21H7Z" fill="currentColor" />
   </Icon>
 );
 
