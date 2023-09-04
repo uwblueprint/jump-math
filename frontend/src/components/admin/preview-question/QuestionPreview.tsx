1f9ebcdf571b693d729ee352821b5089a2ea134e
joyce-shi <joyce.shi@uwaterloo.ca>
2023-09-04 23:05:18 +0200

    feat: render fraction, multiple choice & multi-select ui components

--- frontend/src/components/admin/preview-question/QuestionPreview.tsx
+++ frontend/src/components/admin/preview-question/QuestionPreview.tsx
@@ -2,14 +2,22 @@ import React, { useMemo } from "react";
 import { Box } from "@chakra-ui/react";
 
 import type {
+  FractionMetadata,
   ImageMetadataRequest,
   QuestionTextMetadata,
   TextMetadata,
 } from "../../../types/QuestionMetadataTypes";
-import type { QuestionElement } from "../../../types/QuestionTypes";
+import type {
+  MultiData,
+  MultiOptionData,
+  QuestionElement,
+} from "../../../types/QuestionTypes";
 import { QuestionElementType } from "../../../types/QuestionTypes";
 import { getSubquestionIndexArray } from "../../../utils/StudentUtils";
 import { getSubquestionIndex } from "../../../utils/StudentUtils";
+import FractionInput from "../../common/question-elements/fraction/FractionInput";
+import MultipleChoiceInput from "../../common/question-elements/multi-option/MultipleChoiceInput";
+import MultiSelectInput from "../../common/question-elements/multi-option/MultiSelectInput";
 import Image from "../../student/questions/question-elements/Image";
 import QuestionText from "../../student/questions/question-elements/QuestionText";
 import Text from "../../student/questions/question-elements/Text";
@@ -47,6 +55,48 @@ const QuestionPreview = ({
                   return (
                     <Image
                       url={(element.data as ImageMetadataRequest).previewUrl}
+                    />
+                  );
+                case QuestionElementType.MULTIPLE_CHOICE:
+                  return (
+                    <MultipleChoiceInput
+                      getOption={(option) => (option as MultiOptionData).value}
+                      getOptionValue={(_, index) => index.toString()}
+                      onChange={(e) => console.log(e)} // TODO
+                      options={(element.data as MultiData).options}
+                      value={"0"} // TODO
+                    />
+                  );
+                case QuestionElementType.MULTI_SELECT:
+                  return (
+                    <MultiSelectInput
+                      getOption={(option) => (option as MultiOptionData).value}
+                      getOptionValue={(_, index) => index.toString()}
+                      onChange={(e) => console.log(e)} // TODO
+                      options={(element.data as MultiData).options}
+                      value={["0"]} // TODO
+                    />
+                  );
+                case QuestionElementType.FRACTION: // TODO
+                  if ((element.data as FractionMetadata).wholeNumber !== null) {
+                    return (
+                      <FractionInput
+                        denominator={""}
+                        numerator={""}
+                        onDenominatorChange={(e) => console.log(e)}
+                        onNumeratorChange={(e) => console.log(e)}
+                        onWholeNumberChange={(e) => console.log(e)}
+                        wholeNumber={""}
+                      />
+                    );
+                  }
+                  return (
+                    <FractionInput
+                      denominator={""}
+                      numerator={""}
+                      onDenominatorChange={(e) => console.log(e)}
+                      onNumeratorChange={(e) => console.log(e)}
+                      wholeNumber={null}
                     />
                   );
                 default:
