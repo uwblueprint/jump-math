ea58a422baee9f239673f1c2dd00102f55cdeb17
joyce-shi <joyce.shi@uwaterloo.ca>
2023-09-16 22:46:16 +0200

    refactor: remove user to use common popover

--- frontend/src/components/admin/user-management/RemoveUserPopover.tsx
+++ frontend/src/components/admin/user-management/RemoveUserPopover.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import {
-  Button,
-  IconButton,
-  Popover,
-  PopoverBody,
-  PopoverContent,
-  PopoverTrigger,
-  useDisclosure,
-  VStack,
-} from "@chakra-ui/react";
+import { useDisclosure, VStack } from "@chakra-ui/react";
 
-import { MoreVerticalOutlineIcon } from "../../../assets/icons";
+import Popover from "../../common/popover/Popover";
 
 import RemoveUserModal from "./RemoveUserModal";
 
@@ -26,35 +17,10 @@ const RemoveUserPopover = ({
   const { onOpen, onClose, isOpen } = useDisclosure();
 
   return (
-    <Popover
-      isOpen={isOpen}
-      onClose={onClose}
-      onOpen={onOpen}
-      placement="right"
-    >
-      <PopoverTrigger>
-        <IconButton
-          aria-label="Show more"
-          icon={<MoreVerticalOutlineIcon boxSize={5} />}
-          size="sm"
-        />
-      </PopoverTrigger>
-      <PopoverContent borderColor="grey.300" borderRadius="12px" width="3xs">
-        <PopoverBody>
-          <VStack spacing="0em">
-            <RemoveUserModal email={email} name={name} />
-            <Button
-              bg="blue.200"
-              mb={2}
-              onClick={onClose}
-              opacity="0.6"
-              variant="primary"
-            >
-              Cancel
-            </Button>
-          </VStack>
-        </PopoverBody>
-      </PopoverContent>
+    <Popover isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
+      <VStack spacing="0em">
+        <RemoveUserModal email={email} name={name} />
+      </VStack>
     </Popover>
   );
 };
