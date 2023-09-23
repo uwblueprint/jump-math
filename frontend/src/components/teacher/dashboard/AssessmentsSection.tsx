ce2564339bdbca734f6b7acf97a844e722e32510
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-09-23 12:26:36 -0400

    fix: change teacher dashboard tab design to standard tabs (#580)
    * fix: change teacher dashboard tab design to standard tabs
    
    * PR feedback: extract common component
    
    * fix: extra unnecessary padding
    
    * PR feedback: merge types
    
    * PR feedback: remove unneeded type
    
    * PR feedback: add missing field to type

--- frontend/src/components/teacher/dashboard/AssessmentsSection.tsx
+++ frontend/src/components/teacher/dashboard/AssessmentsSection.tsx
@@ -1,24 +1,12 @@
 import React, { useMemo, useState } from "react";
-import {
-  Box,
-  Center,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-} from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 
 import * as Routes from "../../../constants/Routes";
-import {
-  TEST_SESSION_STATUSES,
-  TestSessionStatus,
-} from "../../../types/TestSessionTypes";
-import { titleCase } from "../../../utils/GeneralUtils";
+import { TestSessionStatus } from "../../../types/TestSessionTypes";
 import ErrorState from "../../common/info/ErrorState";
 import LoadingState from "../../common/info/LoadingState";
 import EmptySessionsMessage from "../../common/info/messages/EmptySessionsMessage";
-import TestSessionListItem from "../view-sessions/TestSessionListItem";
+import TestSessionTabs from "../view-sessions/TestSessionTabs";
 import useAssessmentDataQuery from "../view-sessions/useAssessmentDataQuery";
 
 import ViewAllLink from "./ViewAllLink";
@@ -60,40 +48,10 @@ const AssessmentsSection = () => {
       )}
       {!!data?.length && !error && !loading && (
         <>
-          <Tabs
-            onChange={(index) => setCurrentTab(TEST_SESSION_STATUSES[index])}
-          >
-            <TabList border="none" gap={8}>
-              {TEST_SESSION_STATUSES.map((status) => (
-                <Tab
-                  key={status}
-                  _focus={{ background: "none" }}
-                  border="none"
-                  color="blue.100"
-                  fontWeight="bold"
-                  px={0}
-                >
-                  {titleCase(status)}
-                </Tab>
-              ))}
-            </TabList>
-            <TabPanels>
-              {TEST_SESSION_STATUSES.map((status) => (
-                <TabPanel key={status} p={0}>
-                  {sortedData?.map((session) => (
-                    <TestSessionListItem
-                      key={session.testSessionId}
-                      session={session}
-                    />
-                  ))}
-                </TabPanel>
-              ))}
-            </TabPanels>
-          </Tabs>
+          <TestSessionTabs data={sortedData} setCurrentTab={setCurrentTab} />
           <ViewAllLink
             borderRadius="8px"
             h="50px"
-            mt={4}
             to={Routes.DISPLAY_ASSESSMENTS_PAGE}
           />
         </>
