382e97b91c15127c28dac72e5a40e47d4dd1dc48
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-10-22 14:30:18 -0400

    Update student name selection to use common component

--- frontend/src/types/SelectInputTypes.ts
+++ frontend/src/types/SelectInputTypes.ts
@@ -1,6 +1,5 @@
 import type { OptionBase as ChakraOptionBase } from "chakra-react-select";
 
-import type { StudentResponse } from "../APIClients/types/ClassClientTypes";
 import type { Grade } from "../APIClients/types/UserClientTypes";
 
 export interface OptionBase<T = unknown> extends ChakraOptionBase {
@@ -13,5 +12,3 @@ export type StringOrBoolOption = OptionBase<string | boolean>;
 export type StringOption = OptionBase<string>;
 
 export type GradeOption = OptionBase<Grade>;
-
-export type StudentOption = OptionBase<StudentResponse>;
